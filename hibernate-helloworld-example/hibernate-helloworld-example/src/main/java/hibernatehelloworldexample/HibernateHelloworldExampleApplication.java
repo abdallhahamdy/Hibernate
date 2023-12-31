623ec071c657e34c5ec3963d70f8653cac9f49c9
abdallhahamdy <abdallhahamdy@yahoo.com>
2023-12-31 11:35:47 +0200

    Hibernate Example

---
+++ hibernate-helloworld-example/hibernate-helloworld-example/src/main/java/hibernatehelloworldexample/HibernateHelloworldExampleApplication.java
@@ -0,0 +1,13 @@
+package hibernatehelloworldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHelloworldExampleApplication.class, args);
+	}
+
+}
---
+++ hibernate-helloworld-example/hibernate-helloworld-example/src/test/java/hibernatehelloworldexample/HibernateHelloworldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hibernatehelloworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHelloworldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
